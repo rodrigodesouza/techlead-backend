02b8f24226e4861c5ddc2676017f23e6fe2cc4d0
rodrigo souza <rd7.rodrigo@gmail.com>
2021-01-08 23:25:32 -0300

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Artisan;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->command->info('Criando usuário de acesso');
+        $email = 'contato@admin.com';
+        $password = '12345678';
+        $user = User::updateOrCreate([
+            'email' => $email,
+        ],
+        [
+            'name' => 'Administrador',
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+        $this->command->info('Login: ' . $email);
+        $this->command->info('Senha: ' . $password);
+        // (new Artisan:class)->info($exitCode);
+    }
+}
